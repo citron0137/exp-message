dfb2485aaadfc768525e2fb24aa48f95a1114682
DoHoon Ra <citron0137@gmail.com>
2026-01-11 19:54:09 +0900

    어플리케이션 메소드 로직 테스트로 작성성

--- 02-backend/00-monolitic/src/main/kotlin/site/rahoon/message/__monolitic/authtoken/application/AuthTokenApplicationService.kt
+++ 02-backend/00-monolitic/src/main/kotlin/site/rahoon/message/__monolitic/authtoken/application/AuthTokenApplicationService.kt
@@ -3,34 +3,46 @@ package site.rahoon.message.__monolitic.authtoken.application
 import org.springframework.stereotype.Service
 import site.rahoon.message.__monolitic.authtoken.domain.AccessToken
 import site.rahoon.message.__monolitic.authtoken.domain.AuthToken
-import site.rahoon.message.__monolitic.authtoken.domain.AuthTokenDomainService
+import site.rahoon.message.__monolitic.user.domain.UserDomainService
 
 /**
  * AuthToken Application Service
  * 어플리케이션 레이어에서 도메인 서비스를 호출하고 결과를 반환합니다.
  */
 @Service
-class AuthTokenApplicationService{
+class AuthTokenApplicationService(
+    private val userDomainService: UserDomainService,
+){
 
-    // Login
-    fun login(){
+    // Check
+    fun checkAccessToken( accessToken: String ): AccessToken {
+        // return authTokenDomainService.verifyAccessToken(accessToken)
         TODO()
     }
 
-    // Check
-    fun checkAccessToken(){
+    // Login
+    fun login( email: String, password: String ): AuthToken {
+        // 이메일/비밀번호 검증 -> 토큰 발급 -> 반환
+        val user = userDomainService.getUser( email, password )
+        // return authTokenDomainService.issueToken(user.id)
         TODO()
     }
 
     // Refresh
-    fun refresh(){
+    fun refresh( refreshToken: String ): AuthToken {
+        // val refreshTokenClaim = authTokenDomainService.verifyRefreshToken(refreshToken)
+        // val newToken = authTokenDomainService.issueToken(
+        //      refreshTokenClaim.userId,
+        //      refreshTokenClaim.sessionId
+        // )
+        // authTokenDomainService.expireRefreshToken(refreshToken)
         TODO()
     }
 
     // Logout
-    fun logout(){
+    fun logout( sessionId: String ){
+        // authTokenDomainService.expireBySessionId(sessionId)
         TODO()
     }
-
 }
 
--- 02-backend/00-monolitic/src/main/kotlin/site/rahoon/message/__monolitic/authtoken/application/AuthTokenCriteria.kt
+++
@@ -1,9 +0,0 @@
-package site.rahoon.message.__monolitic.authtoken.application
-
-import jakarta.validation.constraints.NotBlank
-import site.rahoon.message.__monolitic.authtoken.domain.AuthTokenCommand
-
-/** AuthToken Application Layer 입력 DTO */
-object AuthTokenCriteria {
-}
-
