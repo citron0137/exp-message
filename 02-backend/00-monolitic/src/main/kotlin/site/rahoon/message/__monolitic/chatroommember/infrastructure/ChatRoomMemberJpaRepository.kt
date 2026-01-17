6d1ac636c118f2bc23b6fecca20e26765ffd1f24
DoHoon Ra <citron0137@gmail.com>
2026-01-17 13:55:37 +0900

    Add ChatRomm SoftDelete

--- 02-backend/00-monolitic/src/main/kotlin/site/rahoon/message/__monolitic/chatroommember/infrastructure/ChatRoomMemberJpaRepository.kt
+++ 02-backend/00-monolitic/src/main/kotlin/site/rahoon/message/__monolitic/chatroommember/infrastructure/ChatRoomMemberJpaRepository.kt
@@ -3,12 +3,13 @@ package site.rahoon.message.__monolitic.chatroommember.infrastructure
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import site.rahoon.message.__monolitic.common.infrastructure.JpaSoftDeleteRepository
 import java.time.LocalDateTime
 
 /**
  * Spring Data JPA Repository
  */
-interface ChatRoomMemberJpaRepository : JpaRepository<ChatRoomMemberEntity, String> {
+interface ChatRoomMemberJpaRepository : JpaSoftDeleteRepository<ChatRoomMemberEntity, String> {
     fun findByChatRoomIdAndUserId(chatRoomId: String, userId: String): ChatRoomMemberEntity?
     fun findByChatRoomId(chatRoomId: String): List<ChatRoomMemberEntity>
     fun findByUserId(userId: String): List<ChatRoomMemberEntity>
