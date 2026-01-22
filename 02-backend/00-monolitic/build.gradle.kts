c2fc248dfeb8cbab8db03447f7993ae338b85969
DoHoon Ra <citron0137@gmail.com>
2026-01-22 17:54:06 +0900

    Redisson Config 추가 (Pubsub 편리하게 사용하기 위함)

--- 02-backend/00-monolitic/build.gradle.kts
+++ 02-backend/00-monolitic/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.redisson:redisson-spring-boot-starter:3.37.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-aop")
@@ -37,6 +38,12 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+
+    // TODO 코루틴 도입
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.2")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.10.2")
+
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
@@ -53,6 +60,7 @@ dependencies {
     testImplementation("com.redis:testcontainers-redis:2.2.2")
     testImplementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.2")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
