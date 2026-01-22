6082f76f60c1b67cc4a25a01acebd77f55aa63ba
DoHoon Ra <citron0137@gmail.com>
2026-01-22 17:53:28 +0900

    Async에 TaskPoolExecutor가 없는 것 확인하여 수정

--- 02-backend/00-monolitic/src/main/kotlin/site/rahoon/message/monolithic/common/global/config/AsyncConfig.kt
+++ 02-backend/00-monolitic/src/main/kotlin/site/rahoon/message/monolithic/common/global/config/AsyncConfig.kt
@@ -1,11 +1,21 @@
 package site.rahoon.message.monolithic.common.global.config
 
+import org.springframework.boot.task.ThreadPoolTaskExecutorBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
-/**
- * @EnableAsync for @Async 메서드 (예: 이벤트 리스너 비동기 처리).
- */
 @Configuration
 @EnableAsync
-class AsyncConfig
+class AsyncConfig(
+    private val builder: ThreadPoolTaskExecutorBuilder
+) {
+
+    @Bean("taskExecutor")
+    fun taskExecutor(): ThreadPoolTaskExecutor {
+        return builder.build()
+    }
+
+}
+
---
+++ 02-backend/00-monolitic/src/test/kotlin/site/rahoon/message/monolithic/common/global/AsyncRunnerIT.kt
@@ -0,0 +1,36 @@
+package site.rahoon.message.monolithic.common.global
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeSameInstanceAs
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import site.rahoon.message.monolithic.common.test.IntegrationTestBase
+
+class AsyncRunnerIT(
+    private val asyncRunner: AsyncRunner
+): IntegrationTestBase() {
+
+    @Test
+    fun getThreadPoolNames(){
+        val threadPoolNames = asyncRunner.getThreadPoolNames()
+        println(threadPoolNames.joinToString(separator = "\n"))
+    }
+
+    @Test
+    fun runAsyncException(){
+        asyncRunner.runAsync { throw RuntimeException("예외발생") }
+    }
+
+    @Test
+    fun supplyAsyncException(){
+        val future = asyncRunner.supplyAsync { throw RuntimeException("예외발생") }
+
+        val exception = shouldThrow<RuntimeException> {
+            future.join()
+        }
+
+        exception.cause?.message shouldBe "예외발생"
+    }
+
+}
